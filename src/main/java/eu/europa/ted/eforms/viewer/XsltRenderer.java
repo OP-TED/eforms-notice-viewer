44a639d276f31a73235c64c4ec4d7ddf84fb80de
Ioannis Rousochatzakis <ioannis.rousochatzakis@publications.europa.eu>
2022-04-05 02:06:11 +0200

    Implemented dynamic lables (TEDEFO-838)

--- src/main/java/eu/europa/ted/eforms/viewer/XsltRenderer.java
+++ src/main/java/eu/europa/ted/eforms/viewer/XsltRenderer.java
@@ -4,6 +4,8 @@ import eu.europa.ted.efx.interfaces.Renderer;
 
 public class XsltRenderer extends IndentedStringWriter implements Renderer {
 
+    static int variableCounter = 0;
+
     public XsltRenderer() {
         super(0);
         this.indent = 10;
@@ -18,8 +20,7 @@ public class XsltRenderer extends IndentedStringWriter implements Renderer {
     public String renderFile(String body, String templates) {
         IndentedStringWriter writer = new IndentedStringWriter(0);
         writer.writeLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-        writer.openTag("xsl:stylesheet",
-                "version=\"2.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:fn=\"http://www.w3.org/2005/xpath-functions\" xmlns:cbc=\"urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2\" xmlns:cac=\"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2\" xmlns:efext=\"http://data.europa.eu/p27/eforms-ubl-extensions/1\" xmlns:efac=\"http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1\" xmlns:efbc=\"http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1\" xmlns:ext=\"urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2\"");
+        writer.openTag("xsl:stylesheet", "version=\"2.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" xmlns:fn=\"http://www.w3.org/2005/xpath-functions\" xmlns:cbc=\"urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2\" xmlns:cac=\"urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2\" xmlns:efext=\"http://data.europa.eu/p27/eforms-ubl-extensions/1\" xmlns:efac=\"http://data.europa.eu/p27/eforms-ubl-extension-aggregate-components/1\" xmlns:efbc=\"http://data.europa.eu/p27/eforms-ubl-extension-basic-components/1\" xmlns:ext=\"urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2\"");
         writer.writeLine("<xsl:output method=\"html\" encoding=\"UTF-8\" indent=\"yes\"/>");
         writer.writeLine("<xsl:variable name=\"labels\" select=\"fn:document('labels.xml')\"/>");
         writer.openTag("xsl:template", "match=\"/\"");
@@ -29,6 +30,7 @@ public class XsltRenderer extends IndentedStringWriter implements Renderer {
         writer.writeLine("section { border: 1px solid black; padding: 5px; }");        
         writer.writeLine(".text { font-size: 12pt; color: blue;}");
         writer.writeLine(".label { font-size: 12pt; color: green; }");
+        writer.writeLine(".dynamic-label { font-size: 12pt; color: orange; }");
         writer.writeLine(".value { font-size: 12pt; color: red; }");
         writer.closeTag("style");
         writer.closeTag("head");
@@ -48,8 +50,17 @@ public class XsltRenderer extends IndentedStringWriter implements Renderer {
     }
 
     @Override
-    public String renderLabelReference(String labelReference) {
-        return String.format("<span class=\"label\"><xsl:value-of select=\"($labels/properties/entry[./@key='%s']/text(), ' Label not found (%s)')[1]\"/></span>", labelReference, labelReference);
+    public String renderLabelFromKey(final String key) {
+        return String.format("<span class=\"label\"><xsl:value-of select=\"($labels/properties/entry[./@key='%s']/text(), ' Label not found (%s)')[1]\"/></span>", key, key);
+    }
+
+    @Override
+    public String renderLabelFromExpression(final String expression) {
+        IndentedStringWriter writer = new IndentedStringWriter(0);
+        String variableName = String.format("label%d", ++variableCounter);
+        writer.writeLine(String.format("<xsl:variable name=\"%s\" select=\"%s\"/>", variableName, expression));
+        writer.writeLine(String.format("<span class=\"dynamic-label\"><xsl:value-of select=\"($labels/properties/entry[./@key=$%s]/text(), concat('Label not found (', $%s, ')'))[1]\"/></span>", variableName, variableName));
+        return writer.toString();
     }
 
     @Override
