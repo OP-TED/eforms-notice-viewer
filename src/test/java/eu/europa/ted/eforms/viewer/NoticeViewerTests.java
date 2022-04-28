647f2adb55d30607faa7d465282804a065037a52
ROUSSY Christophe (OP-EXT) <Christophe.ROUSSY@ext.publications.europa.eu>
2022-04-28 15:59:28 +0200

    TEDEFO-760 testing 1.efx and fixes done to make it pass (also fixed same problem in other efx).

--- src/test/java/eu/europa/ted/eforms/viewer/NoticeViewerTests.java
+++ src/test/java/eu/europa/ted/eforms/viewer/NoticeViewerTests.java
@@ -17,12 +17,37 @@ public class NoticeViewerTests {
   private static final String SDK_VERSION = "latest";
 
   @Test
-  public void testEfxToHtmlFull() {
+  public void testEfxToHtmlX01English() {
     final String language = "en"; // In english.
-    final String noticeXmlFilename = "X02_registration"; // "X02_registration.xml"
+    final String noticeXmlFilename = "X01_EEIG"; // "X01_EEIG.xml"
+    testGenerateHtml(language, noticeXmlFilename);
+  }
+
+  @Test
+  public void testEfxToHtmlX01Greek() {
+    final String language = "el"; // In greek.
+    final String noticeXmlFilename = "X01_EEIG"; // "X01_EEIG.xml"
+    testGenerateHtml(language, noticeXmlFilename);
+  }
+
+  @Test
+  public void testEfxToHtmlX02() {
+    final String language = "en"; // In english.
+    final String noticeXmlFilename = "X02_registration";
+    testGenerateHtml(language, noticeXmlFilename);
+  }
+
+  @Test
+  public void testEfxToHtml1Minimal() {
+    final String language = "en"; // In english.
+    final String noticeXmlFilename = "pin-buyer_24_minimal";
+    testGenerateHtml(language, noticeXmlFilename);
+  }
+
+  private void testGenerateHtml(final String language, final String noticeXmlFilename) {
     final Optional<String> viewIdOpt = Optional.empty(); // Equivalent to not passing any in cli.
     final Path path = NoticeViewer.generateHtmlForUnitTest(language, noticeXmlFilename, viewIdOpt);
-    logger.info("Wrote html file: {}", path);
+    logger.info("TEST: Wrote html file: {}", path);
     final File htmlFile = path.toFile();
     assertTrue(htmlFile.exists());
     // The test would have failed if there were errors, this is what the check is really about.
@@ -32,7 +57,7 @@ public class NoticeViewerTests {
   public void testEfxToXsl() throws IOException {
     final String viewId = "X02";
     final Path xsl = NoticeViewer.buildXsl(viewId, SDK_VERSION);
-    logger.info("Wrote file: {}", xsl);
+    logger.info("TEST: Wrote file: {}", xsl);
     assertTrue(xsl.toFile().exists());
     // The test would have failed if there were errors, this is what the check is really about.
   }
