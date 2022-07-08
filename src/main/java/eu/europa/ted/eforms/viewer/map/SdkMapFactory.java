80fe650a5126cb726a4c19c6fdc03be31c624da2
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-07-08 15:25:04 +0200

    [Multi-SDK]: Implemented SdkMapFactory as an extension of AbstractSdkObjectFactory.

--- src/main/java/eu/europa/ted/eforms/viewer/map/SdkMapFactory.java
+++ src/main/java/eu/europa/ted/eforms/viewer/map/SdkMapFactory.java
@@ -1,88 +1,17 @@
 package eu.europa.ted.eforms.viewer.map;
 
 import java.nio.file.Path;
-import java.text.MessageFormat;
-import java.util.HashMap;
-import java.util.Map;
-import org.atteo.classindex.ClassIndex;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import eu.europa.ted.eforms.sdk.annotation.SdkComponent;
-import eu.europa.ted.eforms.sdk.component.SdkComponentDescriptor;
 import eu.europa.ted.eforms.sdk.component.SdkComponentTypeEnum;
+import eu.europa.ted.eforms.sdk.factory.AbstractSdkObjectFactory;
 import eu.europa.ted.efx.model.SdkCodelist;
 import eu.europa.ted.efx.model.SdkField;
 import eu.europa.ted.efx.model.SdkNode;
 
-public class SdkMapFactory {
-  private static final Logger log = LoggerFactory.getLogger(SdkMapFactory.class);
-
-  private Map<String, Map<SdkComponentTypeEnum, SdkComponentDescriptor<?>>> componentsMap;
-
+public class SdkMapFactory extends AbstractSdkObjectFactory {
   public static final SdkMapFactory INSTANCE = new SdkMapFactory();
 
   private SdkMapFactory() {
-    populateComponents();
-  }
-
-  private void populateComponents() {
-    if (componentsMap == null) {
-      componentsMap = new HashMap<>();
-    }
-
-    ClassIndex.getAnnotated(SdkComponent.class).forEach((Class<?> clazz) -> {
-      SdkComponent annotation = clazz.getAnnotation(SdkComponent.class);
-
-      String[] supportedSdkVersions = annotation.versions();
-      SdkComponentTypeEnum componentType = annotation.componentType();
-
-      for (String sdkVersion : supportedSdkVersions) {
-        Map<SdkComponentTypeEnum, SdkComponentDescriptor<?>> components =
-            componentsMap.get(sdkVersion);
-
-        if (components == null) {
-          components = new HashMap<>();
-          componentsMap.put(sdkVersion, components);
-        }
-
-        SdkComponentDescriptor<?> component =
-            new SdkComponentDescriptor<>(sdkVersion, componentType, clazz);
-        SdkComponentDescriptor<?> existingComponent = components.get(componentType);
-
-        if (existingComponent != null && !existingComponent.equals(component)) {
-          throw new IllegalArgumentException(MessageFormat.format(
-              "More than one components of type [{0}] have been found for SDK version [{1}]:\n\t- {2}\n\t- {3}",
-              componentType, sdkVersion, existingComponent.getImplType().getName(),
-              clazz.getName()));
-        }
-
-        components.put(componentType, component);
-      }
-    });
-  }
-
-  @SuppressWarnings("unchecked")
-  public <T> T getComponentImpl(String sdkVersion, SdkComponentTypeEnum componentType,
-      Class<T> intf) throws InstantiationException {
-    Map<SdkComponentTypeEnum, SdkComponentDescriptor<?>> components = componentsMap.get(sdkVersion);
-    if (components == null) {
-      log.warn("No SDK components found for SDK [{}]. Trying with fallback SDK ({})", sdkVersion,
-          SdkComponent.ANY);
-      sdkVersion = SdkComponent.ANY;
-      components = componentsMap.get(sdkVersion);
-      if (components == null) {
-        throw new IllegalArgumentException(MessageFormat
-            .format("No SDK component implementations found for SDK version [{0}].", sdkVersion));
-      }
-    }
-    SdkComponentDescriptor<T> descriptor =
-        (SdkComponentDescriptor<T>) components.get(componentType);
-    if (descriptor == null) {
-      throw new IllegalArgumentException(
-          MessageFormat.format("No implementation found for component type [{0}] of SDK [{1}]",
-              componentType, sdkVersion));
-    }
-    return descriptor.createInstance();
+    super();
   }
 
   @SuppressWarnings("unchecked")
