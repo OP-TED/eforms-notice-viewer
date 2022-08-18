99705b59fccb9616f4ff71f4759149a7750d392a
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-08-18 12:42:53 +0200

    Show exceptions and stracktraces on the log file, outputting only error messages on the console.

--- src/main/java/eu/europa/ted/eforms/viewer/Application.java
+++ src/main/java/eu/europa/ted/eforms/viewer/Application.java
@@ -1,13 +1,34 @@
 package eu.europa.ted.eforms.viewer;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import picocli.CommandLine;
+import picocli.CommandLine.IExecutionExceptionHandler;
+import picocli.CommandLine.ParseResult;
 
 /**
  * Entry point.
  */
 public class Application {
-  public static void main(String... args) {
-    int exitCode = new CommandLine(new CliCommand()).execute(args);
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(final String... args) {
+    CommandLine cli = new CommandLine(new CliCommand());
+    cli.setExecutionExceptionHandler(new IExecutionExceptionHandler() {
+      @Override
+      public int handleExecutionException(Exception ex, CommandLine commandLine, ParseResult parseResult)
+          throws Exception {
+        logger.error("Error executing the application with arguments [{}]", Arrays.asList(args));
+        logger.debug("Exception thrown:", ex);
+
+        return 0;
+      }
+    });
+
+    int exitCode = cli.execute(args);
     System.exit(exitCode);
   }
 }
