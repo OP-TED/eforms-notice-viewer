d2a5cb6d8cbeae3b1252a484874ae6b58f42cfc8
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-10-06 20:44:09 +0200

    [CLI]: Set default SDK resources root directory when not given as an application argument.

--- src/main/java/eu/europa/ted/eforms/viewer/CliCommand.java
+++ src/main/java/eu/europa/ted/eforms/viewer/CliCommand.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
+import eu.europa.ted.eforms.sdk.SdkConstants;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.IVersionProvider;
@@ -68,7 +69,8 @@ class CliCommand implements Callable<Integer> {
   public Integer call()
       throws IOException, SAXException, ParserConfigurationException, InstantiationException {
     final Path htmlPath = NoticeViewer.generateHtml(language, noticeXmlPath,
-        Optional.ofNullable(viewId), profileXslt, Path.of(sdkResourcesRoot));
+        Optional.ofNullable(viewId), profileXslt,
+        sdkResourcesRoot != null ? Path.of(sdkResourcesRoot) : SdkConstants.DEFAULT_SDK_ROOT);
     logger.info("Created HTML file: {}", htmlPath);
 
     return 0;
