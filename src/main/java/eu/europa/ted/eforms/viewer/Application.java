36d6b162c19dd91b37b5560ed19940b05d02c49a
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2022-12-21 02:56:50 +0100

    [logging]: Added a JUL-to-SLF4J bridge to handle JCS logging.

--- src/main/java/eu/europa/ted/eforms/viewer/Application.java
+++ src/main/java/eu/europa/ted/eforms/viewer/Application.java
@@ -1,10 +1,10 @@
 package eu.europa.ted.eforms.viewer;
 
 import java.util.Arrays;
-
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import picocli.CommandLine;
 import picocli.CommandLine.IExecutionExceptionHandler;
 import picocli.CommandLine.ParseResult;
@@ -16,12 +16,16 @@ public class Application {
   private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(final String... args) {
+    installJulToSlf4jBridge();
+
     CommandLine cli = new CommandLine(new CliCommand());
     cli.setExecutionExceptionHandler(new IExecutionExceptionHandler() {
       @Override
-      public int handleExecutionException(Exception ex, CommandLine commandLine, ParseResult parseResult)
+      public int handleExecutionException(Exception ex, CommandLine commandLine,
+          ParseResult parseResult)
           throws Exception {
-        logger.error("Error executing the application with arguments [{}]. Please see the logs.", Arrays.asList(args));
+        logger.error("Error executing the application with arguments [{}]. Please see the logs.",
+            Arrays.asList(args));
         logger.debug("Exception thrown:", ex);
 
         return 0;
@@ -30,5 +34,11 @@ public class Application {
 
     int exitCode = cli.execute(args);
     System.exit(exitCode);
+  }
+
+  private static void installJulToSlf4jBridge() {
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
+    java.util.logging.Logger.getLogger("").setLevel(Level.FINEST); // Root logger, for example.
   }
 }
