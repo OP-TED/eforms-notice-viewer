44ba7b08e23847245c4aa72e5c6b4f74bc33a1c2
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2023-04-27 13:04:26 +0200

    [TranslationUriResolver]: Added a null check.

--- src/main/java/eu/europa/ted/eforms/viewer/util/xml/TranslationUriResolver.java
+++ src/main/java/eu/europa/ted/eforms/viewer/util/xml/TranslationUriResolver.java
@@ -3,9 +3,11 @@ package eu.europa.ted.eforms.viewer.util.xml;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.Optional;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.URIResolver;
 import javax.xml.transform.dom.DOMSource;
 import org.w3c.dom.Document;
@@ -13,6 +15,10 @@ import org.xml.sax.SAXException;
 import eu.europa.ted.eforms.sdk.SdkConstants;
 import eu.europa.ted.eforms.sdk.resource.SdkResourceLoader;
 
+/**
+ * A custom URI resolver for handling paths of translation files. It resolves them based on the SDK
+ * version and the SDK root path, allowing for the correct loading of labels.
+ */
 public final class TranslationUriResolver implements URIResolver {
   private String sdkVersion;
   private Path sdkRootPath;
@@ -22,23 +28,23 @@ public final class TranslationUriResolver implements URIResolver {
     this.sdkRootPath = sdkRootPath;
   }
 
-  /**
-   * Currently this allows to load the labels.
-   */
   @Override
-  public Source resolve(final String href, final String base) {
-    try (InputStream is = SdkResourceLoader.getResourceAsStream(sdkVersion,
+  public Source resolve(final String href, final String base) throws TransformerException {
+    try (InputStream translationInput = SdkResourceLoader.getResourceAsStream(sdkVersion,
         SdkConstants.SdkResource.TRANSLATIONS, href, sdkRootPath)) {
-      if (is == null) {
-        throw new RuntimeException(
-            String.format("inputStream is null for href=%s, base=%s", href, base));
+      if (translationInput == null) {
+        throw new IllegalArgumentException(
+            String.format("Input stream is null for href=%s, base=%s", href, base));
       }
+
       // DOM parser based.
       final DocumentBuilder builder = SafeDocumentBuilder.buildSafeDocumentBuilderAllowDoctype();
-      final Document document = builder.parse(is);
+      final Document document = Optional.ofNullable(builder.parse(translationInput))
+          .orElseThrow(() -> new TransformerException("The document parser returned null."));
+
       return new DOMSource(document);
     } catch (SAXException | IOException | ParserConfigurationException e) {
-      throw new RuntimeException(e.toString(), e);
+      throw new TransformerException(e.toString(), e);
     }
   }
 }
