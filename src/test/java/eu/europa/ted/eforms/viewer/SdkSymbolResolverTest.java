8e5a186aff7135ac8b970e28db898cf2c2603d70
Evangelos MELETIOU <Evangelos.MELETIOU@ext.publications.europa.eu>
2023-04-28 13:19:40 +0200

    [testing]: Fixed and enabled "SdkSymbolResolverTest".

--- src/test/java/eu/europa/ted/eforms/viewer/SdkSymbolResolverTest.java
+++ src/test/java/eu/europa/ted/eforms/viewer/SdkSymbolResolverTest.java
@@ -1,49 +1,31 @@
 package eu.europa.ted.eforms.viewer;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.nio.file.Path;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import eu.europa.ted.eforms.sdk.SdkConstants;
 import eu.europa.ted.eforms.sdk.SdkSymbolResolver;
 import eu.europa.ted.eforms.sdk.SdkVersion;
-import eu.europa.ted.eforms.sdk.resource.SdkDownloader;
 
-@Disabled // NOTE: this downloads an SDK.
-public class SdkSymbolResolverTest {
-
+class SdkSymbolResolverTest {
   private static final Logger logger = LoggerFactory.getLogger(SdkSymbolResolverTest.class);
 
   private static final Path SDK_RESOURCES_ROOT =
-      Path.of("target", SdkConstants.DEFAULT_SDK_ROOT.toString());
+      Path.of("src", "test", "resources", "symbol-resolver-test");
 
   @Test
-  public void test() throws InstantiationException, IOException {
-    final Path sdkRootPath = SDK_RESOURCES_ROOT;
-
+  void testSymbolResolution() throws InstantiationException {
     final SdkVersion sdkVersion = new SdkVersion("1.0");
-    SdkDownloader.downloadSdk(sdkVersion, sdkRootPath);
 
     final SdkSymbolResolver resolver =
-        new SdkSymbolResolver(sdkVersion.toStringWithoutPatch(), sdkRootPath);
+        new SdkSymbolResolver(sdkVersion.toStringWithoutPatch(), SDK_RESOURCES_ROOT);
 
-    String fieldId = "BT-10-Procedure-Buyer";
+    final String fieldId = "BT-10-Procedure-Buyer";
     final String rootCodelist = resolver.getRootCodelistOfField(fieldId);
+
     logger.info("Found rootCodelist={} for field={}", rootCodelist, fieldId);
-    // 15:33:09.355 [main] INFO e.e.t.e.viewer.SdkSymbolResolverTest - Found
-    // rootCodelist=main-activity for field=BT-10-Procedure-Buyer
 
-    final String expected = "main-activity";
-
-    // The test works but the junit assert itself fails with:
-    // java.lang.NoClassDefFoundError: org/opentest4j/AssertionFailedError
-    // org.junit.jupiter.api.Assertions.assertEquals(expected, rootCodelist);
-
-    if (!expected.equals(rootCodelist)) {
-      throw new AssertionError(
-          String.format("Expected '%s' but found '%s'", expected, rootCodelist));
-    }
+    assertEquals("main-activity", rootCodelist);
   }
 }
